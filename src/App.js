aeaede847482762748d6285de202b453e27e10e2
parkhyemin <minidoo96@gmail.com>
2020-02-13 16:37:54 +0900

    react 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
+import TOC from './components/TOC';
+import Subject from './components/Subject'
+import Content from './components/Content'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, React?
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <Subject title="React" sub="For UI"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
   }
