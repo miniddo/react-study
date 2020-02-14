3010cd8559b75bf371534181525581fe5e3a2054
miniddo <kg3718@naver.com>
2020-02-14 14:37:05 +0900

    mode에 따라 달라지는 화면 내용

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import TOC from './components/TOC';
 import Subject from './components/Subject'
 import Content from './components/Content'
+import Event from './components/Event'
 
 class Test extends Component {
   render() {
@@ -18,7 +19,9 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      mode: 'welcome', // or 'welcome'
       subject: {title: 'WEB', sub: 'world wide web'},
+      welcome: {title: 'Welcome', desc: 'Hello, React!'},
       contents: [
         {id:1, title: 'HTML', desc: 'HTML is HyperText Markup Language'},
         {id:2, title: 'CSS', desc: 'CSS is for design'},
@@ -27,6 +30,16 @@ class App extends Component {
     }
   }
   render() {
+    let _title = null;
+    let _desc = null;
+
+    if(this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read') {
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
     return (
       <div className="App">
         <Subject 
@@ -37,6 +50,7 @@ class App extends Component {
         <TOC data={this.state.contents}></TOC>
         <Content></Content>
         <Test></Test>
+        <Event title={_title} desc={_desc}></Event>
       </div>
     );
   }
